d7740e644e5773cd259b1279f72071314ff7df24
Luca Zampetti <lzampetti@gmail.com>
2021-06-04 10:43:51 +0200

    giorgetti

--- docs/js/main.js
+++ docs/js/main.js
@@ -3915,7 +3915,7 @@ ProductsConfigureComponent.meta = {
     LocomotiveScrollService.update();
   };
 
-  _proto.configureProduct = function configureProduct(event) {
+  _proto.configureProduct = function configureProduct(item) {
     window.location.href = environment.slug.configureProduct;
   };
 
@@ -6920,6 +6920,13 @@ UserForgotComponent.meta = {
   _proto.setView = function setView(view) {
     this.view = view;
     this.pushChanges();
+
+    var _getContext = rxcomp.getContext(this),
+        node = _getContext.node;
+
+    LocomotiveScrollService.scrollTo(node, {
+      offset: -100
+    });
   };
 
   _proto.onViewForgot = function onViewForgot(event) {
